58eb0ffc60797eb7249249428d18b51b954a51c5
challaravik <challa.ravik@gmail.com>
2017-04-04 22:56:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 /*
-* branch cs6460bot. This is created from gtcoursebot master repository.  ** arana9
+* branch cs6460bot. This is created from gtcoursebot master repository.  ** arana9 ** challaravik
 */
 
 'use strict';
