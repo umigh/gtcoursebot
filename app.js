67436073cbddaa793805e1a3e55abe4ed62c1536
Ajay Rana <arana9@gatech.edu>
2017-04-04 22:48:01 -0400

    added name arana9

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 /*
-* branch cs6460bot. This is created from gtcoursebot master repository.
+* branch cs6460bot. This is created from gtcoursebot master repository.  ** arana9
 */
 
 'use strict';
